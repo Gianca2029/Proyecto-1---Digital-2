925201e9a8f5175f4690957a9945d46b88257ee5
Gianca2029 <aegon2029@gmail.com>
2025-08-29 12:00:30 -0600

    config.h

---
+++ config.h
@@ -0,0 +1,9 @@
+#define IO_USERNAME "gianca2029"
+#define IO_KEY "/*no lo pondré o me lo cambiará*/"
+
+#define WIFI_SSID "Valar Morghulis"
+#define WIFI_PASS "Valar Dohaeris"
+
+#include "AdafruitIO_WiFi.h"
+
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
